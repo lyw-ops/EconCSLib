7a09334945df626ee5cd9bc5b3aef26abe680212
Lazyfill <Lyuyuwei@gmail.com>
2026-08-03 02:43:50 +0800

    refactor(efg): migrate PathLawEquivalence controlled path

--- EconCSLib/GameTheory/ExtensiveGame/Observed/PathLawEquivalence.lean
+++ EconCSLib/GameTheory/ExtensiveGame/Observed/PathLawEquivalence.lean
@@ -3,8 +3,8 @@ Copyright (c) 2026 EconCSLib contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
-import EconCSLib.GameTheory.ExtensiveGame.Observed.ControlledDiscreteLawCompat
-import EconCSLib.GameTheory.ExtensiveGame.Observed.ControlledDiscretePathLaw
+import EconCSLib.GameTheory.ExtensiveGame.Observed.Controlled.Compat.DiscreteLaw
+import EconCSLib.GameTheory.ExtensiveGame.Observed.Controlled.Law.DiscretePath
 import EconCSLib.GameTheory.ExtensiveGame.Observed.InfiniteExecution
 
 /-!
