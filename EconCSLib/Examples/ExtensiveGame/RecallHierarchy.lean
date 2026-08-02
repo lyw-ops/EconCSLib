abf72801956b608996f33aa61dabcc6508f5b54f
Lazyfill <Lyuyuwei@gmail.com>
2026-08-03 02:43:48 +0800

    test(efg): migrate RecallHierarchy controlled imports

--- EconCSLib/Examples/ExtensiveGame/RecallHierarchy.lean
+++ EconCSLib/Examples/ExtensiveGame/RecallHierarchy.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 EconCSLib contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
-import EconCSLib.GameTheory.ExtensiveGame.Observed.ControlledInfrastructureCompat
+import EconCSLib.GameTheory.ExtensiveGame.Observed.Controlled.Compat.Infrastructure
 
 /-!
 # Recall-hierarchy regressions
