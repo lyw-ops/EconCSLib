b592168d5dc657a426a96bb9e0758f38deab771c
Lazyfill <Lyuyuwei@gmail.com>
2026-08-03 02:34:58 +0800

    refactor(efg): group controlled infrastructure adapter

--- EconCSLib/GameTheory/ExtensiveGame/Observed/Controlled/Compat/Infrastructure.lean
+++ EconCSLib/GameTheory/ExtensiveGame/Observed/Controlled/Compat/Infrastructure.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 EconCSLib contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
-import EconCSLib.GameTheory.ExtensiveGame.Observed.ControlledInfrastructure.Recall
+import EconCSLib.GameTheory.ExtensiveGame.Observed.Controlled.Infrastructure.Recall
 import EconCSLib.GameTheory.ExtensiveGame.Observed.Quasi
 import EconCSLib.GameTheory.ExtensiveGame.Observed.SignalRecall
 
