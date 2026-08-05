08401ebf480bfced8fabf4c7b04e0af9622578e8
Lazyfill <Lyuyuwei@gmail.com>
2026-08-05 17:30:14 +0800

    refactor(efg): update CompletePlay

--- EconCSLib/GameTheory/ExtensiveGame/Execution/CompletePlay.lean
+++ EconCSLib/GameTheory/ExtensiveGame/Execution/CompletePlay.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 EconCSLib contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
-import EconCSLib.GameTheory.ExtensiveGame.Execution.History
+import EconCSLib.GameTheory.ExtensiveGame.Structural.History
 
 /-!
 # Complete legal plays
